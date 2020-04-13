11e386497385436590237ae5e16b68cde37fc681
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-04-13 14:23:12 +0300

    serve the production build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className="app">
         <p style={{ textAlign: "center" }}>Covid 19 data analysis :)</p>
       </div>
     );
